be35955d022e30969cbae1a3714b89598c07f49c
Captdam <captdam@beardle.com>
2019-11-24 14:50:33 -0500

    Simplify code, add new procedure "register"

--- conoha.class.php
+++ conoha.class.php
@@ -2,60 +2,57 @@
 	/** For Conoha Object Storage Server ONLY **/
 	class Conoha {
 		private $token;
+		private $tenant;
 		private $endpoint;
-
-                //Constructor
-                //$username, $password, $tenant are provided by Conoha
-                //$token: Your last token, if not know, put anything (NOTICE: PASS BY REFERENCE)
-                //$expire: Expire time in UNIX timestamp, usually 24hrs after request token
-                function __construct($username,$password,$tenant,&$token,$expire) {
-                        //Token expire, request new
-			if ($expire < time()) {
-                                //Prepare cURL request
-                                $curl = curl_init();
-        			$header = array('Content-Type: application/json');
-        			$data = array(
-        				'auth' => array(
-        					'passwordCredentials' => array(
-        						'username' => $username,
-        						'password' => $password
-        					),
-        					'tenantId' => $tenant
-        				)
-        			);
-        			$option = array(
-        				CURLOPT_HTTPHEADER	=> $header,
-	        			CURLOPT_URL		=> 'https://identity.tyo1.conoha.io/v2.0/tokens',
-        				CURLOPT_POST		=> true,
-        				CURLOPT_POSTFIELDS	=> json_encode($data),
-        				CURLOPT_RETURNTRANSFER	=> true,
-        			);
-        			curl_setopt_array($curl,$option);
-        			//Send request
-        			$body = curl_exec($curl);
-        			if (curl_error($curl))
-                                        throw new Exception('CURL error: Fail to renew token, error message: '.curl_error($curl).'.');
-        			$status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        			if(curl_errno($curl) || $status_code != 200)
-                                        throw new Exception('CURL error: Bad return code, cURL error: '.curl_errno($curl).'; HTTP: '.$status_code.'.');
-        			$authinfo = json_decode($body);
-                                if (!$authinfo)
-                                        throw new Exception('CURL error: Bad return data.');
-                                //Renew token
-                                $token = $authinfo->access->token->id;
-			}
-			//Token OK (not expired)
-			$this->token = $token;
+		
+		function __construct($tenant,$token=null) {
+			$this->tenant = $tenant;
 			$this->endpoint = 'https://object-storage.tyo1.conoha.io/v1/nc_'.$tenant.'/';
-			//Notice: as long as the token did not expire (24hrs), it is possible to have more than one active token
-                }
+			$this->token = $token;
+		}
+		
+		//Auth: Get token
+		//$auth = array('username'=>username, 'password'=>password)
+		public function register($auth) {
+			$auth = array('auth' => array(
+				'passwordCredentials'	=> array(
+					'username'	=> $auth['username'] ?? '',
+					'password'	=> $auth['password'] ?? ''
+				),
+				'tenantId'		=> $this->tenant
+			));
+			
+			$curl = curl_init();
+			$request = array(
+				CURLOPT_HTTPHEADER	=> array('Content-Type: application/json'),
+				CURLOPT_URL		=> 'https://identity.tyo1.conoha.io/v2.0/tokens',
+				CURLOPT_POST		=> true,
+				CURLOPT_POSTFIELDS	=> json_encode($auth),
+				CURLOPT_RETURNTRANSFER	=> true
+			);
+			curl_setopt_array($curl,$request);
+			
+			$response = curl_exec($curl);
+			if (curl_error($curl))
+				throw new Exception('Fail to renew token: '.curl_error($curl));
+			
+			$responseStatus = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+			if ( curl_errno($curl) || $responseStatus != 200 )
+				throw new Exception('Fail to renew token. CURL-'.curl_errno($curl).' HTTP-'.$responseStatus);
+			
+			$response = json_decode($response,true);
+			if (!$response)
+				throw new Exception ('Bad return data.');
+			
+			$this->token = $response['access']['token']['id'];
+			return $this->token;
+		}
 
 		//PUT
                 //To create a container: $url = 'container_name'
                 //To save a file, $url = 'url', $content = '/local/direction/to/file'
                 //To save an string/blob, $url = 'url', $content = 'string/blob', $stream = false
 		public function put($url,$content=null,$stream=true) {
-			//Create stream
 			if ($content) {
 				if (!$stream) {
 					$temp = tmpfile();
@@ -65,77 +62,48 @@
 				}
 				else $content = fopen($content,'r');
 			}
-			//Prepare request
-			$curl = curl_init();
-			$header = array();
-			$header = array(
-				'X-Auth-Token: '.$this->token
-			);
-			if ($content) $options = array( #Create object
-				CURLOPT_PUT		=> true,
-				CURLOPT_INFILE		=> $content,
-				CURLOPT_URL		=> $this->endpoint.$url,
-				CURLOPT_RETURNTRANSFER	=> true,
-				CURLOPT_HTTPHEADER	=> $header
-			);
-			else $options = array( #Create container
-				CURLOPT_PUT		=> true,
-				CURLOPT_URL		=> $this->endpoint.$url,
-				CURLOPT_VERBOSE		=> true,
-				CURLOPT_RETURNTRANSFER	=> true,
-				CURLOPT_HTTPHEADER	=> $header
-			);
-			curl_setopt_array($curl, $options);
-			//Send request
-			$body = curl_exec($curl);
-                        if (curl_error($curl))
-                                throw new Exception('CURL error: Fail to put content, error message: '.curl_error($curl).'.');
-			return array(curl_getinfo($curl, CURLINFO_HTTP_CODE),$body);
-
+			
+			return $this->com('PUT',$url,$content);
 		}
 
 		//GET
                 //$url: URL to resource, just the local URL, do not include beginning '/', for example 'photo/myphoto.png'
 		public function get($url) {
-			//Prepare request
-			$curl = curl_init();
-			$header = array(
-				'X-Auth-Token: '.$this->token
-			);
-			$options = array(
-				CURLOPT_URL		=> $this->endpoint.$url,
-				CURLOPT_RETURNTRANSFER	=> true,
-				CURLOPT_HTTPHEADER	=> $header
-			);
-			curl_setopt_array($curl, $options);
-			//Send request
-			$body = curl_exec($curl);
-                        if (curl_error($curl))
-                                throw new Exception('CURL error: Fail to get content, error message: '.curl_error($curl).'.');
-			return array(curl_getinfo($curl, CURLINFO_HTTP_CODE),$body);
+			return $this->com('GET',$url);
 		}
 
 		//DELETE
                 //$url: URL to resource, just the local URL, do not include beginning '/', for example 'photo/myphoto.png'
 		public function delete($url) {
-			//Prepare request
+			return $this->com('DELETE',$url);
+		}
+		
+		private function com($method,$url,$post=null) {
 			$curl = curl_init();
-			$header = array(
-				'X-Auth-Token: '.$this->token
-			);
-			$options = array(
-				CURLOPT_CUSTOMREQUEST	=> 'DELETE',
+			$request = array(
 				CURLOPT_URL		=> $this->endpoint.$url,
-				CURLOPT_VERBOSE		=> true,
-				CURLOPT_RETURNTRANSFER	=> true,
-				CURLOPT_HTTPHEADER	=> $header
+				CURLOPT_HTTPHEADER	=> array('X-Auth-Token: '.$this->token),
+				CURLOPT_RETURNTRANSFER	=> true
 			);
-			curl_setopt_array($curl, $options);
-			//Send request
-			$body = curl_exec($curl);
-                        if (curl_error($curl))
-                                throw new Exception('CURL error: Fail to delete content, error message: '.curl_error($curl).'.');
-			return array(curl_getinfo($curl, CURLINFO_HTTP_CODE),$body);
+			switch($method) {
+				case 'GET':
+					/* GET by default */
+					break;
+				case 'PUT':
+					$request[CURLOPT_PUT] = true;
+					if ($post)
+						$request[CURLOPT_INFILE] = $post;
+					break;
+				default:
+					$request[CURLOPT_CUSTOMREQUEST] = $method;
+			}
+			
+			curl_setopt_array($curl, $request);
+			$response = curl_exec($curl);
+			if (curl_error($curl))
+                                throw new Exception(curl_error($curl));
+			
+			return array(curl_getinfo($curl, CURLINFO_HTTP_CODE),$response);
 		}
 	}
 ?>
