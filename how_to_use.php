776c62a38f15ebb830db7e3090edc8875240d2f5
Captdam <captdam@beardle.com>
2019-11-24 15:14:07 -0500

    Update how_to_use.php

--- how_to_use.php
+++ how_to_use.php
@@ -1,48 +1,61 @@
 <?php
-	//Setup the class, two ways, choose one:
-	//1 - Basic way, apply a new token every time. This way is simple, but it takes time to request token everytime
-	$iniStatus = $this->objectStorage->ini('username','password','tenantid','',0);
-	//2 - If you use database to manage token, use this method
-	$token = $DB->getToken();
-	$expireTime = $DB->getexpireTime();
-	$oldToken = $token;
-	$conoha = new Conoha('username','password','tenantid',$token,$expireTime);
-	if ($token != $oldToken) { #Token is passed by reference, it will be changed if token expire
-		$DB->setToken($token);
-		$DB->setExpireTime($time()+72000); #Conoha says token vaild for 24 hrs, here we use 20 hrs
-	}
+	//API account info
+	$token = 'Token issued last time (optional) You can just leave it blank';
+	define('OS_USERNAME',   'gncu000000');
+	define('OS_PASSWORD',   'passwordNeverBroken');
+	define('OS_TENANT',     '0101010101000cacbhadkjsfbjqn0302w9f89');
 	
-	//Now, you are ready to use this class, the following shows how to use this class:
-	//To see the output of each method, using var_dump($x);
-	//Then, your will get an array with 2 keys, where:
-	//$x[0] is the return HTTP code, eg: 200, 404, 409
-	//$x[1] is the return body
-	//Notice that, some method may not return a body. For example, if you using DELETE method, only HTTP satus code 409 will be return.
+	//Create a instance
+	include 'conoha.class.php';
+	$os = new Conoha(OS_TENANT,$token);
 	
-	//GET
+	//If you provide a valid token, this should return HTTP 200
+	echo $os->get('')[0];
+	//otherwise, auth
+	$token = $os->register(array('username'=>OS_USERNAME,'password'=>OS_PASSWORD));
+	save_my_token($token,time());
+	/* The token is valid for 24 hrs, you may want to save it so you don't need to auth again next time */
+
+	//Use the following procedures to PUT/GET/DELETE objects.
+	//All procedures returns an array with 2 elements.
+	//$return[0] <-- HTTP code of the response
+	//$return[1] <-- Response body
+	//Notice that, some methods may not return a body. For example, if you using DELETE method, only HTTP status code 409 will be return.
+	
+	/************************** GET **************************/
 	
 	//GET container list
 	$x = $conoha->get('');
-	//GET file (resource) list in a container
-	$x = $conoha->get('YOUR_CONTAINER_NAME/');
+	//GET file (object) list in a container
+	$x = $conoha->get('CONTAINER_NAME');
 	//GET a file
-	$x = $conoha->get('YOUR_CONTAINER_NAME/THE_RESOURCE NAME');
+	$x = $conoha->get('CONTAINER_NAME/OBJECT_NAME');
 	
-	//PUT
+	/************************** PUT **************************/
 	
 	//Create a container
-	$x = $conoha->put('NEW_CONTAINER_NAME/');
-	//Create a resource (no content)
-	$x = $conoha->put('CONTAINER_NAME/RESOURCE_NAME');
-	//Create a resource, copy from a file
-	$x = $conoha->put('CONTAINER_NAME/RESOURCE_NAME','LOCAL_FILE_NAME');
-	//Create a resource, the content is part of the parameter of this method
-	$x = $os->put('CONTAINER_NAME/RESOURCE_NAME','CONTENT_YOU_WISH_TO_WRITE',true);
+	$x = $conoha->put('NEW_CONTAINER_NAME');
+	//Create an object (no content)
+	$x = $conoha->put('CONTAINER_NAME/NEW_OBJECTE_NAME');
+	//Upload a file from local server to the object storage server
+	$x = $conoha->put('CONTAINER_NAME/NEW_OBJECT_NAME','PATH_TO_LOCAL_FILE_NAME');
+	//Create an object, the content is saved in a variable
+	$x = $os->put('CONTAINER_NAME/NEW_OBJECT_NAME','CONTENT_YOU_WISH_TO_WRITE',false);
+
+	/*
+	Name conflict:
+	If you create a container that is already exist, 202 will be returned with no change on the storage server.
+	If you upload an object that is already exist, 201 will be returned and the old object will be overwriten (no matter content is provided)
+	Hint:
+	It is better to include file name extension in the object name (e.g. $os->put('container1/myImage.jpeg',...))
+	When using web publishing (All objects in a container is avaliable for GET without token),
+	Conoha uses the file name extension to determine MIME in the HTTP response header.
+	*/
 	
-	//DELETE
+	/************************* DELETE ************************/
 	
-	//Delete a resource
-	$x = $conoha->delete('CONTAINER_NAME/RESOURCE_NAME');
-	//Delete a container, notice that you need to delete every thing in the container first
-	$x = $conoha->delete('CONTAINER_NAME/');
+	//Delete an object
+	$x = $conoha->delete('CONTAINER_NAME/OBJECT_NAME');
+	//Delete a container, notice that you need to delete every thing in the container first, otherwise 409 will be returned
+	$x = $conoha->delete('CONTAINER_NAME');
 ?>
